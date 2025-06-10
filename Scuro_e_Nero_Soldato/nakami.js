87e13b0f879dbb9783d9bb13cf51583d7b82f773
koppepan-orange <koppepanorange@gmail.com>
2025-06-10 16:38:44 +0900

    そめてぃんg

--- Scuro_e_Nero_Soldato/nakami.js
+++ Scuro_e_Nero_Soldato/nakami.js
@@ -92,53 +92,161 @@ async function zako(){
     await delay(100);
 }
 //#endregion
+//#region data
+let Cards = {
+    //normal
+    'A':{
+        name:'A',
+        val:'A',
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    '2':{
+        name:'2',
+        val:2,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    '3':{
+        name:'3',
+        val:3,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    '4':{
+        name:'4',
+        val:4,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    '5':{
+        name:'5',
+        val:5,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    '6':{
+        name:'6',
+        val:6,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    '7':{
+        name:'7',
+        val:7,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    '8':{
+        name:'8',
+        val:8,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    '9':{
+        name:'9',
+        val:9,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    '10':{
+        name:'10',
+        val:10,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    'J':{
+        name:'J',
+        val:10,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    'Q':{
+        name:'Q',
+        val:10,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    'K':{
+        name:'K',
+        val:10,
+        suits:['♡','♤','♢','♧'],
+        kind:'normal',
+        rare:'N',
+        buyable:1,
+    },
+    'strength':{
+        name:'strength',
+        val:0,
+        suits:'Ⅷ',
+        kind:'tarot',
+        rare:'UR',
+        buyable:1
+    },
+    'wheel of fourtune':{
+        name:'wheel of fourtune',
+        val:0,
+        suits:'Ⅹ',
+        kind:'tarot',
+        rare:'UR',
+        buyable:1
+    }
+}
+//#endregion
 
 let humans = {
     player:{
         name: "player",
         cards: [],
         value: 0,
-        win: 0,
-        lose: 0,
-        draw: 0,
-        blue: 0, //blackJackになった数
     },
     dealer:{
+        deck: [
+            {
+                name:'A',
+                value:'A',
+                suit:'♡',
+                data:{
+                    name:'A',
+                    val:'A',
+                    suits:['♡','♤','♢','♧'],
+                    kind:'normal',
+                    rare:'N',
+                    buyable:1,
+                }
+            }
+        ],
         cards: [],
         value: 0,
-        win: 0,
-        lose: 0,
-        draw: 0,
-        blue: 0,
     }
 }
-let euro = 100;
-let bet = 1;
-let betOrigin = 5;
-let standed = 0;
-let wholeRate = 0; //負けた時に加算されるやつ
+let coin = 100;
 
-let betZone = document.getElementById("betZone");
-let betDisplay = betZone.querySelector('.bet')
-let betRange = betZone.querySelector(".range");
-betRange.addEventListener('input',() => {
-    betTekiou();
-})
-function betTekiou(){
-    bet = betRange.value;
-    betDisplay.textContent = `bet:${bet}`;
-}
-let betDecide = betZone.querySelector(".button");
-betDecide.addEventListener('click', function(){
-    if(this.value < 1) return zako();
-    bet = this.value;
-    euro -= bet;
-    //betZone.style.left = '-40%';
-    betZone.style.display = 'none';
-    uiButtons.style.display = 'flex';
-})
-betTekiou();
-
+let spada = 21; //仮名。
 
 let uiButtons = document.getElementById('buttons')
 let hitButton = uiButtons.querySelector(".hit");
@@ -170,61 +278,95 @@ function tekiou(){
     });
     
 }
-function getCard(){
-    let x = Math.floor(Math.random() * 13)+1;
-    let y = x;
-    switch(y){
-        case 1: y = "A";break;
-        case 11: y = "J";break;
-        case 12: y = "Q";break;
-        case 13: y = "K";break;
-    }
-    if(x > 10){
-        x = 10;
-    }
-    let z = arraySelect(["♡","♤","♢","♧"])
-    return [x,y,z];
+function getCard(cam){
+    let deck = humans[cam].deck.filter(a => !a.showing);
+    let card = arraySelect(deck); // ランダムに1枚選ぶ関数だと仮定
+    card.showing = 1;
+    return [card.name, card.value, card.suit];
 }
 
-hitButton.addEventListener("click", ()=>{
-    if(humans["player"].value <= 20 || standed){
-        let [num, card, suit] = getCard('player');
-        cardAdd("player", num, card, suit);
-        tekiou();
+hitButton.addEventListener("click", async function (){ 
+    let player = humans['player'];
+    let dealer = humans['dealer']
+    if(!player.standed){
+        let drawCard = cardDraw('player')
+        await delay(750);
+        if(player.value > spada){
+            let isburst = isBurst('player');
+            if(isburst) return;
+        }
 
-        if(humans["player"].value > 21){
-            //when barst, suddenly lose
-            outcome('dealer')
+        if(!dealer.standed){
+            drawCard = cardDraw('dealer');
+            await delay(750);
+            if(dealer.value > spada){
+                let isburst = isBurst('dealer');
+                if(isburst) return;
+            }
         }
     }
 })
 
-function cardAdd(cam, value, card, suit){
-    humans[cam].cards.push({
+function cardDraw(cam){
+    let [name, num, suit] = getCard(cam);
+    cardAdd(cam, name, num, suit);
+    tekiou();
+
+
+}
+function cardAdd(cam, name, value, suit){
+    let cardData = Cards[name]
+    
+    let drawCard = {
+        name: name,
         value: value,
-        card: card,
-        suit: suit
-    })
+        suit: suit,
+        data: cardData
+    }
+    
+    humans[cam].cards.push(drawCard);
 
     let newCard = document.createElement("div");
-    newCard.className = "card";
+    newCard.className = `card ${cardData.kind}`;
 
-    let upper = document.createElement("div");
-    upper.className = "upper";
-    upper.textContent = suit
-    newCard.appendChild(upper)
+    switch(cardData.kind){
+        case 'normal':{
+            let upper = document.createElement("div");
+            upper.className = "upper";
+            upper.textContent = suit
+            newCard.appendChild(upper)
 
-    let num = document.createElement("div");
-    num.className = "num";
-    num.textContent = card;
-    newCard.appendChild(num)
+            let num = document.createElement("div");
+            num.className = "num";
+            num.textContent = name;
+            newCard.appendChild(num)
 
-    let lower = document.createElement("div");
-    lower.className = "lower";
-    lower.textContent = suit
-    newCard.appendChild(lower)
+            let lower = document.createElement("div");
+            lower.className = "lower";
+            lower.textContent = suit
+            newCard.appendChild(lower)
+
+            break;
+        };
+        case 'tarot':{
+            let upper = document.createElement("div");
+            upper.className = "upper";
+            upper.textContent = suit
+            newCard.appendChild(upper);
+            
+            let img = document.createElement("img");
+            img.className = 'img';
+            console.log(`assets/cards/${cardData.kind}/${cardData.id}`)
+            img.src = `assets/cards/${cardData.kind}/${cardData.id}.png`;
+            newCard.appendChild(img);
+            'assets/cards/tarot/wheel_of_fourtune'
+            
+            break;
+        }
+    };
     
-    document.querySelector(`#cardPlaces .${cam}`).appendChild(newCard)
+    document.querySelector(`#cardPlaces .${cam}`).appendChild(newCard);
+    return drawCard;
 }
 
 standButton.addEventListener("click", async function (){
@@ -247,21 +389,7 @@ standButton.addEventListener("click", async function (){
 async function result(){
     let player = humans["player"]
     let dealer = humans["dealer"]
-    if(dealer.value > 21){
-        return "player";
-    }else if(player.value == 21){
-        if(dealer.value == 21){
-            return player.cards.length < dealer.cards.length ? "player" : "dealer";
-        }else{
-            return "player";
-        }
-    }else if(player.value > dealer.value){
-        return "player";
-    }else if(player.value > dealer.value){
-        return "dealer";
-    }else if(player.value == dealer.value){
-        return player.cards.length < dealer.cards.length ? "player" : "dealer";
-    }
+
 }
 async function outcome(winner){
     let thisRate = 1;
@@ -269,7 +397,7 @@ async function outcome(winner){
         case 'player':
             wholeRate = 0;
             thisRate = (player.value == 21 ? 3 : 2 + wholeRate);
-            euro += Math.floor(bet * thisRate);
+            coin += Math.floor(bet * thisRate);
             break;
         case 'dealer':
             wholeRate += 0.2;
@@ -320,14 +448,27 @@ function battleStart(){
     tekiou();
 
     wholeRate = 0;
-    betRange.value = betOrigin;
-    betRange.max = euro;
-    betTekiou();
-    
-    betZone.style.display = 'block';
-    uiButtons.style.display = 'none';
 }
 
 
 //一旦のやつ
 battleStart();
+
+//本来はダンジョン開始時にやるべき動きを！なんと贅沢にコードの1番下に描かせていただきます！！
+let nums = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+let suits = ['♡','♤','♢','♧'];
+humans.player.deck = [];
+humans.dealer.deck = [];
+nums.forEach(num => {
+    suits.forEach(suit => {
+        const card = {
+            name: num,
+            value: Cards[num].val,
+            suit: suit,
+            showing: 0,
+            data: Cards[num]
+        };
+        humans.player.deck.push(card);
+        humans.dealer.deck.push(card);
+    });
+});
