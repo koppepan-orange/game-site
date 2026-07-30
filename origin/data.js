3ece57cb3770536866c4d8732b2648ce806cd56a
koppepan-orange <koppepanorange@gmail.com>
2026-07-31 07:05:56 +0900

    新js, komagome？

--- origin/data.js
+++ origin/data.js
@@ -2,12 +2,11 @@ let Style = {
     iPhone:{ //16
         "width": "393px",
     },
-    tekiou: function() {
-        for (let section in this) {
-            if (section == 'apply') continue;
-            for (let key in this[section]) {
-                document.documentElement.style
-                    .setProperty(`--${section}-${key}`, this[section][key]);
+    tekiou: function(){
+        for(let section in this){
+            if(section == 'tekiou') continue;
+            for(let key in this[section]){
+                document.documentElement.style.setProperty(`--${section}-${key}`, this[section][key]);
             }
         }
     }
